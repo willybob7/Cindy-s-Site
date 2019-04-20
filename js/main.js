4d997e0ee6ff846458a35fef6fe7c2e090cac657
willybob7 <34104463+willybob7@users.noreply.github.com>
2019-04-19 21:52:06 -0400

    added slideshow fadin/fadeout

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
 var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+var i;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    //add a opacity transition
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 4000);
+  slides[slideIndex - 1].style.display = "block";
+  
+  // setTimeout(showSlides, 4000);
+  setTimeout(fadeOut, 4000);
+
+}
+
+var opacity = 1
+
+function fadeIn() {
+  if (opacity > 1) {
+    setTimeout(fadeOut, 4000);
+  } else {
+    opacity += 0.02;
+    slides[slideIndex - 1].style.opacity = opacity;
+    setTimeout(fadeIn, 5)
+  }
+}
+
+function fadeOut() {
+  if (opacity <= 0) {
+    clearInterval(setInterval);
+    slides[slideIndex - 1].style.display = "none";
+    slideIndex++
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.opacity = opacity;
+    slides[slideIndex - 1].style.display = "block";
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex - 1].className += " active";
+    fadeIn()
+  } else {
+    opacity -= 0.02;
+    slides[slideIndex - 1].style.opacity = opacity;
+    setTimeout(fadeOut, 5)
+  }
 }
